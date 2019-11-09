d4ae1b1d703c810350f41e541ec3d8da4cbdbe56
Alex <alex-korn@rambler.ru>
2019-11-09 12:47:32 +0300

    done Factorial почему-то приходитс вводить число дважды

---
+++ src/main/java/com/ifmo/lesson2/Factorial.java
@@ -0,0 +1,36 @@
+package com.ifmo.lesson2;
+
+import java.util.Scanner;
+
+public class Factorial {
+    /*
+     Создайте программу, вычисляющую факториал натурального числа n, которое
+     пользователь введёт с клавиатуры.
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        long factorial = factorial(n);
+        System.out.println(factorial);
+
+    }
+
+    private static long factorial(int n) {
+        // TODO implement
+        int a;
+        long b = 1;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите натуральное число");
+        //Проверка, число введенное натуральное или нет
+        if (scanner.hasNextInt()) {
+            //Число натуральное, переменной а присваивается значение введенного числа
+            a = scanner.nextInt();
+            // вычисление факториала
+            for (int i = a; i > 0; i--) {
+                b *= i;
+            }
+            System.out.println("Факториал числа " + a + " равен " + b);
+        } else System.out.println("Ошибка. Введено не число");
+        return b;
+    }
+}
