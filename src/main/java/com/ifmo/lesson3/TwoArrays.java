cf118360e8be5773b08ea23c85d9b63a75226aed
Alex <alex-korn@rambler.ru>
2019-12-05 16:51:40 +0300

    done 3

--- src/main/java/com/ifmo/lesson3/TwoArrays.java
+++ src/main/java/com/ifmo/lesson3/TwoArrays.java
@@ -1,5 +1,9 @@
 package com.ifmo.lesson3;
 
+import java.util.Arrays;
+import java.util.OptionalDouble;
+import java.util.Random;
+
 public class TwoArrays {
     /*
      Создайте 2 массива из 5 случайных целых чисел из отрезка [0;5] каждый, выведите
@@ -7,27 +11,48 @@ public class TwoArrays {
      каждого массива и сообщите, для какого из массивов это значение оказалось больше (либо
      сообщите, что их средние арифметические равны).
      */
+
     public static void main(String[] args) {
         int[] randomNumbers1 = randomNumbers();
+        printArray(randomNumbers1);
         int[] randomNumbers2 = randomNumbers();
-
-        // TODO implement
+        printArray(randomNumbers1);
 
         int average1 = average(randomNumbers1);
         int average2 = average(randomNumbers2);
 
-        // TODO implement
+        if (average1 == average2) {
+            System.out.println("Значения равны");
+        } else if (average1 > average2) {
+            System.out.println("Масив 1 больше Масива 2");
+        } else {
+            System.out.println("Масив 2 больше масива 1");
+        }
     }
-
+    public static Random random = new Random();
     public static int[] randomNumbers() {
-        // TODO implement
 
-        return new int[0];
+        int[] array = new int[5];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(6);
+        }
+        return array;
     }
 
     public static int average(int[] randomNumbers) {
-        // TODO implement
 
-        return 0;
+        OptionalDouble averageDouble = Arrays.stream(randomNumbers).average();
+        return (int) Math.round(averageDouble.getAsDouble());
+    }
+
+    private static void printArray(int[] array) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < array.length; i++) {
+            builder.append(array[i]);
+            if (i < array.length - 1) {
+                builder.append(" ");
+            }
+        }
+        System.out.println(builder);
     }
 }
