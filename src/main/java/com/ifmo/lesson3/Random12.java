a1969105e406834a64fdb10bcf79b5e598b8fb85
Alex <alex-korn@rambler.ru>
2019-12-05 17:19:29 +0300

    done 3

--- src/main/java/com/ifmo/lesson3/Random12.java
+++ src/main/java/com/ifmo/lesson3/Random12.java
@@ -1,37 +1,46 @@
 package com.ifmo.lesson3;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.util.Random;
+
 public class Random12 {
     /*
     Создайте массив из 12 случайных целых чисел из отрезка [-15;15]. Определите какой
     элемент является в этом массиве максимальным и сообщите индекс его последнего
     вхождения в массив.
      */
+    public static Random random = new Random();
+
     public static void main(String[] args) {
         int[] randomNumbers = randomNumbers();
-
-        // TODO implement
-
         int max = max(randomNumbers);
         int maxLastIndex = lastIndexOf(randomNumbers, max);
-
-        // TODO implement
+        System.out.println(maxLastIndex);
     }
 
     public static int[] randomNumbers() {
-        // TODO implement
-
-        return new int[0];
+        int[] array = new int[12];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(31) - 15;
+        }
+        return array;
     }
 
     public static int max(int[] randomNumbers) {
-        // TODO implement
-
-        return 0;
+        int max = randomNumbers[0];
+        for (int i = 1; i < randomNumbers.length; i++) {
+            max = Math.max(max, randomNumbers[i]);
+        }
+        return max;
     }
 
     public static int lastIndexOf(int[] randomNumbers, int max) {
-        // TODO implement
-
-        return 0;
+        int index = 0;
+        for (int i = 0; i < randomNumbers.length; i++) {
+            if (randomNumbers[i] == max) {
+                index = i;
+            }
+        }
+        return index;
     }
 }
