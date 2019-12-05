a02a4c8b08c400b29b74bde2043bc12355f577c6
Alex <alex-korn@rambler.ru>
2019-12-05 17:30:12 +0300

    done 3

--- src/main/java/com/ifmo/lesson3/Random8.java
+++ src/main/java/com/ifmo/lesson3/Random8.java
@@ -1,30 +1,47 @@
 package com.ifmo.lesson3;
 
+import java.util.Random;
+
 public class Random8 {
     /*
     Создайте массив из 8 случайных целых чисел из отрезка [1;10]. Выведите массив на экран
     в строку. Замените каждый элемент с нечётным индексом на ноль. Снова выведете массив на
     экран на отдельной строке.
      */
+    private static Random random = new Random();
+
     public static void main(String[] args) {
         int[] randomNumbers = randomNumbers();
+        printArray(randomNumbers);
+        int[] replaceWithZeros = replaceWithZeros(randomNumbers);
+        printArray(replaceWithZeros);
 
-        // TODO implement
-
-        int[] replacedWithZeros = replaceWithZeros(randomNumbers);
-
-        // TODO implement
     }
 
     public static int[] randomNumbers() {
-        // TODO implement
-
-        return new int[0];
+        int[] array = new int[8];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(10) + 1;
+        }
+        return array;
     }
 
     public static int[] replaceWithZeros(int[] randomNumbers) {
-        // TODO implement
-
-        return new int[0];
+        for (int i = 1; i < randomNumbers.length; i++) {
+            randomNumbers[i] = 0;
+        }
+        return randomNumbers;
     }
+
+    public static void printArray(int[] array) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < array.length; i++) {
+            builder.append(array[i]);
+            if (i < array.length - 1) {
+                builder.append(" ");
+            }
+        }
+        System.out.println(builder);
+    }
+
 }
