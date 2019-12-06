0e3f8fbabb875e91e9c804bad9351279a1098efa
Alex <alex-korn@rambler.ru>
2019-12-06 10:41:22 +0300

    fix2: Big...Random

--- src/main/java/com/ifmo/lesson2/BiggestInRandom.java
+++ src/main/java/com/ifmo/lesson2/BiggestInRandom.java
@@ -1,5 +1,7 @@
 package com.ifmo.lesson2;
 
+import java.util.Random;
+
 public class BiggestInRandom {
     /*
      Создать программу, выводящую на экран случайно сгенерированное трёхзначное
@@ -8,7 +10,10 @@ public class BiggestInRandom {
      В числе 774 наибольшая цифра 7.
      В числе 613 наибольшая цифра 6.
      */
+    public static Random random = new Random();
+
     public static void main(String[] args) {
+
         int rnd = threeDigitRandom();
 
         String largestDigit = largestDigit(rnd);
@@ -17,26 +22,23 @@ public class BiggestInRandom {
     }
 
     public static int threeDigitRandom() {
-        // TODO implement
-        double r = Math.random()*1000;
-        return (int) r;
+        return (random.nextInt(899) + 100);
     }
 
-
-    public static String largestDigit (int rnd) {
-            int max = 0;
+    public static String largestDigit(int rnd) {
+        int max = 0;
         // TODO implement
-            int a = rnd / 100;
-            int b = (rnd - a * 100) / 10;
-            int c = rnd - (a * 100 + b * 10);
+        int a = rnd / 100;
+        int b = (rnd - a * 100) / 10;
+        int c = rnd - (a * 100 + b * 10);
 
-            if ((a > b) & (a > c))
-                max = a;
-            if ((b > a) & (b > c))
-                max = b;
-            else {
-                max = c;
-            }
+        if ((a > b) & (a > c))
+            max = a;
+        if ((b > a) & (b > c))
+            max = b;
+        else {
+            max = c;
+        }
         return "В числе " + rnd + " наибольшая цифра " + max + ".";
     }
 }
