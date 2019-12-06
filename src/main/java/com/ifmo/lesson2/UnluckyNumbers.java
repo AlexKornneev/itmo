7265413875b09e2f9ad34de60a3a807371981584
Alex <alex-korn@rambler.ru>
2019-12-06 11:27:18 +0300

    fix2: NonNegativeSequence

--- src/main/java/com/ifmo/lesson2/UnluckyNumbers.java
+++ src/main/java/com/ifmo/lesson2/UnluckyNumbers.java
@@ -15,11 +15,12 @@ public class UnluckyNumbers {
 
     public static int unluckyNumbersCount() {
         int a = 0;
-        for (int i = 4; i < 99994; i++) {
+        for (int i = 0; i < 99999; i++) {
             String s = String.valueOf(i);
             if (s.indexOf('4') != -1) {
                 a++;
-            } else if (s.indexOf('1') != -1 & s.indexOf('1') + 1 == s.indexOf('3')) {
+            }
+            if (s.indexOf('1') != -1 & s.indexOf('1') + 1 == s.indexOf('3')) {
                 a++;
             }
         }
