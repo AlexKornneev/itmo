a7be4d413341aec5d3e4c1bcedbb70b27113c407
Alex <alex-korn@rambler.ru>
2019-12-06 12:01:10 +0300

    fix2: Factorial

--- src/main/java/com/ifmo/lesson2/Factorial.java
+++ src/main/java/com/ifmo/lesson2/Factorial.java
@@ -12,25 +12,14 @@ public class Factorial {
         int n = scanner.nextInt();
         long factorial = factorial(n);
         System.out.println(factorial);
-
     }
 
     public static long factorial(int n) {
-        // TODO implement
-        int a;
         long b = 1;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите натуральное число");
-        //Проверка, число введенное натуральное или нет
-        if (scanner.hasNextInt()) {
-            //Число натуральное, переменной а присваивается значение введенного числа
-            a = scanner.nextInt();
-            // вычисление факториала
-            for (int i = a; i > 0; i--) {
-                b *= i;
-            }
-            System.out.println("Факториал числа " + a + " равен " + b);
-        } else System.out.println("Ошибка. Введено не число");
-        return b;
+        for (int i = n; i > 0; i--) {
+            b *= i;
+        }
+        System.out.println("Факториал числа " + n + " равен " + b);
+        return n;
     }
 }
