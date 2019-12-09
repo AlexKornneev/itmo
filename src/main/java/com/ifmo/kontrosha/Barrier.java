ddc3d458e452b99583ea59bd65d8484624555397
Alex <alex-korn@rambler.ru>
2019-12-09 10:07:32 +0300

    fix: kontrosha 07.12.2019

--- src/main/java/com/ifmo/kontrosha/Barrier.java
+++ src/main/java/com/ifmo/kontrosha/Barrier.java
@@ -1,50 +1,92 @@
 package com.ifmo.kontrosha;
 
+import java.util.Random;
+
 public class Barrier implements Runnable {
+    private static final int THREADS = 3;
+
+    private static class Await {
+        public boolean set = false;
+    }
+
+    private static final Await[] awaits = new Await[THREADS];
+    private static final Thread[] threads = new Thread[THREADS];
+    private static Random rnd = new Random();
+    private final int threadNum;
+
+    private Barrier(int num) {
+        this.threadNum = num;
+    }
+
+    public static int getRndNumber() {
+        return rnd.nextInt(1000) * 10;
+    }
+
+    public void run() {
+        synchronized (awaits) {
+            awaits[this.threadNum].set = true;
+            awaits.notify();
+        }
+
+        final long startExecution = System.currentTimeMillis();
+        executeTask();
+        final long finishExecution = System.currentTimeMillis();
+        System.out.println("Поток N" + this.threadNum + ". запущен в : " +
+                startExecution + ". окончен в: " + finishExecution +
+                ". Выполнялся: " + (finishExecution - startExecution) + ".");
+    }
+
+    public void executeTask() {
+        int temp = 1000;
+        for (int i = 0; i < getRndNumber(); i++)
+            for (int j = 0; j < getRndNumber(); j++)
+                for (int k = 0; k < getRndNumber(); k++)
+                    temp = temp / temp * temp;
+    }
 
     public static void main(String[] args) {
+        for (int i = 0; i < THREADS; i++) {
+            awaits[i] = new Await();
+        }
 
-        MyThread3 thread1 = new MyThread3("MyThread1");
-        thread1.start();
-        MyThread3 thread2 = new MyThread3("MyThread2");
-        thread2.start();
-        MyThread3 thread3 = new MyThread3("MyThread3");
-        thread3.start();
-    }
+        System.out.println("Запускаем " + THREADS + " потока...");
 
-    @Override
-    public void run() {
+        synchronized (Barrier.class) {
+            synchronized (awaits) {
+                for (int i = 0; i < THREADS; i++) {
+                    final Barrier mainThread = new Barrier(i);
+                    threads[i] = new Thread(mainThread, "Поток N" + i);
+                    threads[i].start();
+                }
 
-    }
-}
+                System.out.println("Ждем статра у барьера ...");
 
-class MyThread3 implements Runnable {
-    Thread mythread;
-    private String threadname;
+                boolean isAllStarted = false;
+                while (!isAllStarted) {
+                    try {
+                        awaits.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
 
-    MyThread3(String name) {
-        threadname = name;
-    }
-
-    @Override
-    public void run() {
-        System.out.println("Thread running" + threadname);
-        for (int i = 0; i < 3; i++) {
-            System.out.println(i);
-            System.out.println(threadname);
-            try {
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                System.out.println("Thread has been interrupted");
+                    isAllStarted = true;
+                    for (int i = 0; i < THREADS; i++) {
+                        if (!awaits[i].set) {
+                            isAllStarted = false;
+                            break;
+                        }
+                    }
+                }
             }
         }
-    }
 
-    public void start() {
-        System.out.println("Thread started");
-        if (mythread == null) {
-            mythread = new Thread(this, threadname);
-            mythread.start();
+        for (int i = 0; i < THREADS; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                return;
+            }
         }
     }
 }
